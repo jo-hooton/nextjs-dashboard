7f3b757e18876eda4dbde035ebb695dbb652bdf8
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-11-18 17:54:22 +0000

    add create invoice form

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -13,7 +13,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   }
